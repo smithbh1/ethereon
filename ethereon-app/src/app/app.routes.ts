b744dde5e49d37717cde9823044f895801577b74
dKaiona <14drob35@gmail.com>
2024-04-09 11:23:53 -0600

    started on the login component. We might be missing a file called app.module.ts

--- ethereon-app/src/app/app.routes.ts
+++ ethereon-app/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
+import { ChatViewComponent } from './chat-view/chat-view.component';
 import { LoginComponent } from './login/login.component';
-
 export const routes: Routes = [
+    {path: 'chat', component: ChatViewComponent},
     { path: 'login', component: LoginComponent },
 ];
