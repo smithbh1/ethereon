a04e0e8ca7992850b90584c1c432d04c1fa16fdc
Casey Johnson <caseyajohnson10@gmail.com>
2024-04-16 09:35:24 -0600

    api creation

--- ethereon-app/src/app/dashboard/dashboard.component.ts
+++ ethereon-app/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -6,8 +8,24 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrls: ['./dashboard.component.scss']  
 })
+
 export class DashboardComponent {
 
+  public getJsonValue: any;
+  public PostJsonValue: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+   this.getMethod();
+  }
+  public getMethod(){
+    this.http.get('https://jsonplaceholder.typicode.com/posts/1').subscribe((data)=> {console.log(data)
+    this.getJsonValue = data;
+    }
+    );
+    
+  }
 }
